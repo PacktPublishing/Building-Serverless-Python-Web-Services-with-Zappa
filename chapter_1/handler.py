9bfb9ae6bcd1294191c916709d87ddade8da5163
Abdul Wahid <abdulwahid24@gmail.com>
2018-03-12 00:51:18 +0530

    Chapter one code.

---
+++ chapter_1/handler.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    return "Hello World ! Response by lambda function."
+
+
+
---
+++ chapter_1/lambda_function.py
@@ -0,0 +1,11 @@
+from __future__ import print_function 
+import json 
+ 
+print('Loading function') 
+ 
+def lambda_handler(event, context): 
+    print("Received event: " + json.dumps(event, indent=2)) 
+    print("value1 = " + event['key1']) 
+    print("value2 = " + event['key2']) 
+    print("value3 = " + event['key3']) 
+    return "Hello World" 
