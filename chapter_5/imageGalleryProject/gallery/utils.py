9d0ade5c59efb3a248c9cca03fe0184146819269
Abdul Wahid <abdulwahid24@gmail.com>
2018-04-09 15:43:12 +0530

    Updated intendation

--- chapter_5/imageGalleryProject/gallery/utils.py
+++ chapter_5/imageGalleryProject/gallery/utils.py
@@ -8,8 +8,8 @@ class StaticStorage(S3BotoStorage):
     def connection(self):
         if self._connection is None:
             self._connection = self.connection_class(
-                    self.access_key, self.secret_key,
-                    calling_format=self.calling_format, host='s3-ap-south-1.amazonaws.com')
+                self.access_key, self.secret_key,
+                calling_format=self.calling_format, host='s3-ap-south-1.amazonaws.com')
         return self._connection
 
 
